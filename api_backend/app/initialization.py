4cf227f2dc0be4d0fdf02a49da96483d1dffa449
Michal <m.marciniak@neurosys.com>
2020-11-28 20:10:26 +0100

    Add endpoints for test app

--- api_backend/app/initialization.py
+++ api_backend/app/initialization.py
@@ -1,20 +1,18 @@
 import os
 import pandas as pd
 
+data_path = os.path.join('./'+os.getcwd(),'data')
+
 def initialize():
     df = pd.read_csv(os.path.join(data_path, 'dataset.csv'))
     # find all unique diseases
     diseases = get_unique_diseases(df)
     # get unique symptoms for diseases
-    
+    diseases_symptoms = get_diseases_symptoms(df,diseases)
+    # get all symptoms
+    all_symptoms = get_all_symptoms(df)
 
-    all_symptoms = set()
-    for i in range(1, 18):
-        for symptom in df[f"Symptom_{i}"].unique():
-            if pd.isnull(symptom):
-                continue
-            all_symptoms.add(symptom.strip())
-
+    return [diseases,diseases_symptoms,all_symptoms]
 
 def get_unique_diseases(df):
     return df['Disease'].unique()
@@ -29,3 +27,12 @@ def get_diseases_symptoms(df,diseases):
                     continue
                 diseases_symptoms[disease].add(symptom.strip())
     return diseases_symptoms
+
+def get_all_symptoms(df):
+    all_symptoms = set()
+    for i in range(1, 18):
+        for symptom in df[f"Symptom_{i}"].unique():
+            if pd.isnull(symptom):
+                continue
+            all_symptoms.add(symptom.strip())
+    return all_symptoms
--- api_backend/app/main.py
+++ api_backend/app/main.py
@@ -3,10 +3,17 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.responses import HTMLResponse,JSONResponse
 import pandas as pd
 import os
+from .initialization import initialize
+import numpy as np
+import random
+
 
 app = FastAPI()
 
-data_path = os.path.join('./'+os.getcwd(),'data')
+init_state = initialize()
+
+diseases,diseases_symptoms,all_symptoms = init_state[0],init_state[1],init_state[2]
+
 
 @app.get("/")
 async def main():
@@ -15,5 +22,34 @@ async def main():
 @app.get("/diseases")
 async def get_diseases():
     return {"diseases":list(diseases)}
+
+@app.get("/symptoms")
+async def get_all_symptoms():
+    return {"symptoms":list(all_symptoms)}
+
+@app.get("/disease")
+async def get_disease_symptoms(disease: str):
+    return {"symptoms": list(diseases_symptoms[disease])}
+
+@app.get("/question")
+async def get_quiz_symptoms(disease : str):
+    num_disease_symptoms = len(diseases_symptoms[disease])
+    num_of_true = random.randint(0, num_disease_symptoms)
+    true_symptoms = random.sample(diseases_symptoms[disease],num_of_true)
+    num_of_false = 10 - num_of_true
+    all_symptoms_list = list(all_symptoms)
+    false_symptoms = []
+    i = 0
+    while i < num_of_false:
+        rnd_symptom = random.choice(all_symptoms_list)
+        if rnd_symptom in true_symptoms or rnd_symptom in false_symptoms:
+            continue
+        false_symptoms.append(rnd_symptom)
+        i += 1
+
+    return {"true_symptoms":true_symptoms,
+            "false_symptoms":false_symptoms,
+            "true_num":len(true_symptoms),
+            "false_num":len(false_symptoms)}
 
 
