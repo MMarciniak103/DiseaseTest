f6197f46e5944b51dad502524e1b1593e7a4eeac
Michal <michal.marciniak103@gmail.com>
2020-11-30 21:01:41 +0100

    Add firebase registration functionality

--- app/src/main/java/com/mmarciniak/diseasetest/RegistrationActivity.kt
+++ app/src/main/java/com/mmarciniak/diseasetest/RegistrationActivity.kt
@@ -2,22 +2,46 @@ package com.mmarciniak.diseasetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.register_view.*
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_view)
+
+        auth = FirebaseAuth.getInstance()
+
 
         register_button.setOnClickListener {
             if (email_input.text.trim().toString().isNotEmpty() && password_input.text.trim()
                     .toString().isNotEmpty()
             ) {
-
+                createUser(email_input.text.trim().toString(),password_input.text.trim().toString())
             }
             else {
-
+                Toast.makeText(this,"You must provide both email and password",Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun createUser(email: String, password: String){
+        auth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){task ->
+                if(task.isSuccessful)
+                {
+                    runOnUiThread{
+                        Toast.makeText(this,"Account registered successfully",Toast.LENGTH_SHORT).show()
+                    }
+                }
+                else {
+                    runOnUiThread{
+                        Toast.makeText(this,"Account registration failed ${task.exception}",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
     }
 }
