a46c60a36d5555bed3ba5e8e9d35d9580f93486a
Michal <michal.marciniak103@gmail.com>
2020-12-12 18:16:56 +0100

    Update regex that checks if answer contained all words

--- app/src/main/java/com/mmarciniak/diseasetest/Test2Activity.kt
+++ app/src/main/java/com/mmarciniak/diseasetest/Test2Activity.kt
@@ -5,13 +5,16 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mmarciniak.diseasetest.api.DiseaseApiManager
+import com.mmarciniak.diseasetest.fragments.OnQuizCompleteListener
+import com.mmarciniak.diseasetest.fragments.QuizResultDialogFragment
+import com.mmarciniak.diseasetest.fragments.Test2ResultDialogFragment
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_test.*
 import kotlinx.android.synthetic.main.activity_test2.*
 import java.io.IOException
 import java.util.*
 
-class Test2Activity : AppCompatActivity() {
+class Test2Activity : AppCompatActivity(), OnQuizCompleteListener {
     private val apiManager = DiseaseApiManager()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,7 +36,7 @@ class Test2Activity : AppCompatActivity() {
         val randomDisease = diseasesList.random()
         apiManager.selectedDisease = randomDisease
         println("[SECOND TEST] Selected Disease: $randomDisease")
-        apiManager.getDiseaseSymptoms(::handleDiseaseSymptoms,randomDisease)
+        apiManager.getDiseaseSymptoms(::handleDiseaseSymptoms, randomDisease)
     }
 
 
@@ -46,23 +49,38 @@ class Test2Activity : AppCompatActivity() {
     }
 
     private fun submitAnswer() {
-        if (disease_input.text.trim().toString().isNotEmpty())
-        {
+        if (disease_input.text.trim().toString().isNotEmpty()) {
             val diseaseName = disease_input.text.trim().toString().toLowerCase(Locale.getDefault())
             val goodAnswer = apiManager.selectedDisease?.trim()?.toLowerCase(Locale.getDefault())
             goodAnswer?.let {
-                val words = goodAnswer.split(" ").toList()
-                val rx = Regex("\\b(?:${words.joinToString(separator="|")})\\b")
-                val matches = rx.containsMatchIn(diseaseName)
-                if(matches)
-                {
+                val match = checkIfMatch(goodAnswer, diseaseName)
+                val dialog = Test2ResultDialogFragment.newInstance(goodAnswer, match)
+                dialog.show(supportFragmentManager, "test2ResultDialog")
+            }
+        } else {
+            Toast.makeText(this, "You need to enter disease name", Toast.LENGTH_SHORT).show()
+        }
+    }
 
-                }
+    private fun checkIfMatch(goodAnswer: String, diseaseName: String): Boolean {
+        val words = goodAnswer.split(" ").toList()
+        val rx = Regex("\\b(?:${words.joinToString(separator = "|")})\\b")
+        val matchesNum = rx.findAll(diseaseName)
+        var match = false
+        if (matchesNum.count() == words.size) match = true
+        return match
+    }
+
+    override fun onComplete(reset: Boolean) {
+        if (!reset) {
+            disease_input.text.clear()
+            try {
+                apiManager.getListOfDiseases(::chooseRandomDisease)
+            } catch (e: IOException) {
+                println(e.printStackTrace())
             }
         }
-        else{
-            Toast.makeText(this,"You need to enter disease name",Toast.LENGTH_SHORT).show()
-        }
+
     }
 
 }
---
+++ app/src/main/java/com/mmarciniak/diseasetest/fragments/Test2ResultDialogFragment.kt
@@ -0,0 +1,81 @@
+package com.mmarciniak.diseasetest.fragments
+
+import android.app.Activity
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.mmarciniak.diseasetest.R
+import kotlinx.android.synthetic.main.test2_result_dialog.view.*
+import java.lang.IllegalStateException
+
+class Test2ResultDialogFragment : DialogFragment() {
+
+    private var diseaseName: String = ""
+    private var match: Boolean = false
+
+    private lateinit var mListener: OnQuizCompleteListener
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        diseaseName =
+            arguments?.getString(diseaseNameKey) ?: throw IllegalStateException("No args provided")
+        match = arguments?.getBoolean(matchKey) ?: throw IllegalStateException("No args provided")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView: View = inflater.inflate(R.layout.test2_result_dialog, container, false)
+        if (match) {
+            rootView.test2_result_label.text = "You were right"
+        } else {
+            rootView.test2_result_label.text = "You were wrong"
+        }
+        val diseaseLabelText ="disease name was:\n $diseaseName"
+        rootView.test2_tv.text = diseaseLabelText
+
+        rootView.go_back_button.setOnClickListener {
+            mListener.onComplete(false)
+            dismiss()
+        }
+
+        rootView.show_drugs_btn.setOnClickListener {
+            mListener.onComplete(true)
+            dismiss()
+        }
+
+        return rootView
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try{
+            mListener = activity as OnQuizCompleteListener
+        }
+        catch (e: ClassCastException)
+        {
+            throw ClassCastException(activity.toString() + " must implement OnCompleteListener")
+        }
+    }
+
+
+    companion object {
+        private const val diseaseNameKey = "diseaseName"
+        private const val matchKey = "match"
+
+        @JvmStatic
+        fun newInstance(diseaseName: String, match: Boolean): Test2ResultDialogFragment =
+            Test2ResultDialogFragment().apply {
+                arguments = Bundle().apply {
+                    putString(diseaseNameKey, diseaseName)
+                    putBoolean(matchKey, match)
+                }
+            }
+    }
+}
